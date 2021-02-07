e89af799adf4e22a028da949f9362576d7ae72e7
renee-coder <71918527+renee-coder@users.noreply.github.com>
2021-02-07 10:28:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+objects = [];
 video= "";
 status = "";
 
@@ -13,6 +14,29 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+
+        for (i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are: "+objects.length;
+
+            fill("red");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+ "%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function start(){
